5cb23f22cfb067cdcd3148c595883c97e47510bd
Sergio Caue Gomes <sgomes@RDL800879.lan>
2022-02-18 20:52:11 -0300

    Commit Exercicio 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Primeiro from './components/primeiro'
+import CompPadrao, {Component1, Component2} from './components/Multi.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Primeiro></Primeiro>
+    <h2>Primeiro Componente pasta app.</h2>  
     </div>
   );
 }
 
 export default App;
+
+//FuncãoAnonima e exportando direto
+//export default function(){
+  //return <h3>Primeiro Componente</h3>
+//}
+
+//Usando Arrow Function
+//export defaut () => {
+  //return <h2>Primeiro Componente</h2>
+//}
+
+//Usando arrow Function com o return Implicito
+//export defaut () => <h2>Primeiro Componente</h2>
+
